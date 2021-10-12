52f24b4dc0defc7baee7df64d251cc48b823c97d
rpgarde <rpgarde@gmail.com>
2021-10-13 00:05:58 +1100

    Revert "initial db schema built, seeds not working yet"
    This reverts commit 03f1c6ca8017a14318bcda45cbc104f006ec35d9.

--- models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    House: require("./House"),
-    User: require("./User"),
-    Message: require("./Message"),
-    Chore: require("./Chore"),
-    Bill: require("./Bill")
-  };
--- seeds/houses.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    {
-        address: "111/99a Bonar Street Wolli Creek 2205",
-        photo: "house.jpg",
-        users: [1,2]
-    }
-];
