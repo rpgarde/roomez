7e6743b046c33c38b04e345b516f73e6d678ca30
rpgarde <rpgarde@gmail.com>
2021-11-04 22:10:35 +1100

    updated server and procfile

--- server/server.js
+++ server/server.js
@@ -28,7 +28,12 @@ app.use(morgan('tiny'))
 
 app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static("public"));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+// Step 2:
+// app.get("/", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// });
 
 app.use(graphqlUploadExpress());
 
