3636f17337cd024cbfcbbf3983ff0f655357513b
rpgarde <rpgarde@gmail.com>
2021-11-04 23:27:58 +1100

    add wildcard for heroku

--- server/server.js
+++ server/server.js
@@ -34,9 +34,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 // Step 2:
-// app.get("/", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-// });
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 
 db.on('error', console.error.bind(console, 'connection error:'));
