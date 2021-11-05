a86a747782a931e957126027e6edd4006f03afa6
rpgarde <rpgarde@gmail.com>
2021-11-05 19:04:30 +1100

    tweaked server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -141,7 +141,7 @@ const resolvers = {
       // Invoking the `createReadStream` will return a Readable Stream.
       // See https://nodejs.org/api/stream.html#stream_readable_streams
       const stream = createReadStream();
-      const pathName = path.join(__dirname, '..', '..', `client/build/public/images/${filename}`)
+      const pathName = path.join(__dirname, '..', '..', `client/build/images/${filename}`)
       console.log(pathName)
       // This is purely for demonstration purposes and will overwrite the
       // local-file-output.txt in the current working directory on EACH upload.
